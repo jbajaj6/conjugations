2e37fe67463d6a9bc40f5f5de7193ffcd1d3e85d
Jon Salenger <jbsalenger@outlook.com>
2020-09-26 20:29:47 -0400

    Firebase integration now working

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,15 @@
 </svelte:head>
 
 <script>
+	/*
+		c_ = correct
+		l_ = local ( within the scope of a function)
+	*/
 	import * as firebase from "firebase/app";
-
 	import 'firebase/firestore';
-	
-	var firebaseConfig = {
-		apiKey: "AIzaSyAyvaVVZs8goGeUch0lvhQhlo4Ika0WyGY",
-		authDomain: "conjugations-8df51.firebaseapp.com",
-		databaseURL: "https://conjugations-8df51.firebaseio.com",
-		projectId: "conjugations-8df51",
-		storageBucket: "conjugations-8df51.appspot.com",
-		messagingSenderId: "792949381129",
-		appId: "1:792949381129:web:ae7f5e00984e5fdae380d8",
-		measurementId: "G-71MBDCTS7P"
-	};
-	// Initialize Firebase
-	firebase.initializeApp(firebaseConfig);
-	const db = firebase.firestore();
-	let verbs = getVerbs();
+
+	const verbs = getVerbs();
+	let index = 0;
 
 	let conjugations = ['', '', '', '', '', ''];
 	const forms = ['Yo', 'Tu', 'El/Ella/Usted', 'Nosotros', 'Vosotros', 'Ellos/Ellas/Ustedes'];
@@ -28,10 +19,30 @@
 	let correct = true;
 
 	async function getVerbs() {
-		/* move this into a cloud function at some point */
-		const verbsRef = db.collection('verbs').doc('poner');
-		const verb = await verbsRef.get();
-		return verb;
+		var firebaseConfig = {
+			apiKey: "AIzaSyAyvaVVZs8goGeUch0lvhQhlo4Ika0WyGY",
+			authDomain: "conjugations-8df51.firebaseapp.com",
+			databaseURL: "https://conjugations-8df51.firebaseio.com",
+			projectId: "conjugations-8df51",
+			storageBucket: "conjugations-8df51.appspot.com",
+			messagingSenderId: "792949381129",
+			appId: "1:792949381129:web:ae7f5e00984e5fdae380d8",
+			measurementId: "G-71MBDCTS7P"
+		};
+
+		firebase.initializeApp(firebaseConfig);
+		const db = firebase.firestore();
+
+		let verbsRef = await db.collection('verbs').get();
+		let l_verbs = [];
+
+		verbsRef.forEach(v => {
+			l_verbs = [...l_verbs, v.data()];
+			l_verbs = [...l_verbs, v.data()];
+		});
+
+		console.log(l_verbs);
+		return l_verbs.sort((a, b) => 0.5 - Math.random());
 	}
 	
 	function handleSubmit() {
@@ -39,31 +50,41 @@
 		let l_correct = true;
 		let l_responses = [];
 		
-		conjugations.forEach((c, i) => {
-			c = c.replaceAll("^a", "á");
-			c = c.replaceAll("^e", "é");
-			c = c.replaceAll("^i", "í");
-			c = c.replaceAll("^o", "ó");
-			c = c.replaceAll("^u", "ú");
-			c = c.replaceAll("^^u", "ü");
-			c = c.replaceAll("^n", "ñ");
+		verbs.then(v => {
+			let c_conjugations = v[index].indicPresent;
 
-			if(c !== verbs[0].indicPresent[i]) {
-				l_correct = false;
-				l_responses = l_responses.concat([`${c === "" ? "blank" : c} should be ${verbs[0].indicPresent[i]}`]);
+			conjugations.forEach((c, i) => {
+				c = c.replaceAll("^a", "á");
+				c = c.replaceAll("^e", "é");
+				c = c.replaceAll("^i", "í");
+				c = c.replaceAll("^o", "ó");
+				c = c.replaceAll("^u", "ú");
+				c = c.replaceAll("^^u", "ü");
+				c = c.replaceAll("^n", "ñ");
+
+				if(c !== c_conjugations[i]) {
+					l_correct = false;
+					l_responses = l_responses.concat([`${c === "" ? "blank" : c} should be ${c_conjugations[i]}`]);
+				}
+			});
+
+			correct = l_correct;
+			responses = l_responses;
+			index += 1;
+
+			if (index === v.length) {
+				index = 0;
+				console.log("Completed Verbs")
 			}
 		});
-
-		correct = l_correct;
-		responses = l_responses;
 	}
 </script>
 
 <main>
 	{#await verbs}
 		<h1>Verb: Loading...</h1>
-	{:then verbs} 
-		<h1>Verb: {verbs.data().name}</h1>
+	{:then verbs}
+		<h1>Verb: {verbs[index].name}</h1>
 	{/await}
 	<p>Your last answer was {correct ? "correct" : "incorrect"}</p>
 	{#if !correct}
